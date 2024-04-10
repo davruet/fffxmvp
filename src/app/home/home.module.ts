a19864a5137d225e6a4ca6358d2a4cffbe45950e
drueter <drueter@gmail.com>
2024-04-10 22:22:34 +0200

    New features as demoed

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 import { ItemListComponent } from '../item-list/item-list.component';
 import { ImageAccordionComponent } from '../image-accordion/image-accordion.component';
+import { HttpClientModule } from '@angular/common/http';
 
 
 
@@ -16,7 +17,8 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    HttpClientModule
   ],
   declarations: [HomePage, ItemListComponent, ImageAccordionComponent]
 })
