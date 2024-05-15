498d502813a5e80b32f99de2de783c32f7b6ecf3
davruet <david@davidrueter.com>
2024-05-15 22:56:48 +0200

    Fix startup 502 error

---
+++ backend/app/run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Start Gunicorn
+gunicorn --workers 1 --timeout 120 --log-level=debug -b :5000 app:app &
+
+# Function to check if Gunicorn is ready
+check_gunicorn() {
+    python3 << END
+import socket
+import sys
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    sock.connect(("localhost", 5000))
+except socket.error:
+    sys.exit(1)
+sock.close()
+sys.exit(0)
+END
+}
+
+# Wait for Gunicorn to be ready
+until check_gunicorn; do
+  echo "Waiting for Gunicorn..."
+  sleep 0.1
+done
+
+# Start Nginx
+nginx -g 'daemon off;'
